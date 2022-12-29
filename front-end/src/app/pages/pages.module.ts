3e02e176bb43592edb564133773e8d9a793d0706
Fabio Yamashita <yamashitafabio@gmail.com>
2022-12-28 22:47:06 -0300

    feat: Add login page (HTML, CSS)

--- front-end/src/app/pages/pages.module.ts
+++ front-end/src/app/pages/pages.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { LoginComponent } from './login/login.component';
+import { SharedModule } from '../shared/shared.module';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [LoginComponent],
+  imports: [CommonModule, SharedModule, MaterialModule, ReactiveFormsModule],
+  exports: [LoginComponent],
 })
 export class PagesModule {}
