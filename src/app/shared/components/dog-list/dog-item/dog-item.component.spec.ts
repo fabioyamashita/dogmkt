2b8fe0d210106085421107e7d746b0ac4128e3fd
Fabio Yamashita <yamashitafabio@gmail.com>
2023-02-18 17:01:24 -0300

    test: Add dog item component unit tests setup

--- src/app/shared/components/dog-list/dog-item/dog-item.component.spec.ts
+++ src/app/shared/components/dog-list/dog-item/dog-item.component.spec.ts
@@ -1,4 +1,8 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MatIconModule } from '@angular/material/icon';
+import Dog from 'src/app/models/dog';
 
 import { DogItemComponent } from './dog-item.component';
 
@@ -6,14 +10,34 @@ describe('DogItemComponent', () => {
   let component: DogItemComponent;
   let fixture: ComponentFixture<DogItemComponent>;
 
+  const dog: Dog = {
+    id: 7,
+    availableQuantity: 10,
+    name: 'The Akita',
+    breed: 'Japanese Akita',
+    genre: 'Female',
+    price: 240.5,
+    description: 'The Akita.',
+    dateOfBirth: '2023-01-02',
+    weight: 14,
+    height: 25,
+    width: 25,
+    pictureUrl:
+      'https://images.unsplash.com/photo-1613410180211-106788bd2d94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
+    sellerId: 3,
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DogItemComponent ]
-    })
-    .compileComponents();
+      declarations: [DogItemComponent],
+      imports: [MatIconModule, RouterTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DogItemComponent);
     component = fixture.componentInstance;
+
+    component.dog = dog;
+  
     fixture.detectChanges();
   });
 
