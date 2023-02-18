81be8da8e54ce6e976a5032c9127eb489fdb9808
Fabio Yamashita <yamashitafabio@gmail.com>
2023-02-17 22:50:50 -0300

    refactor: refactoring dog-details component using operators and SRP

--- src/app/shared/components/dog-details/dog-details.component.ts
+++ src/app/shared/components/dog-details/dog-details.component.ts
@@ -1,7 +1,8 @@
-import { RoutesService } from '../../../services/routes.service';
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { tap, switchMap, finalize } from 'rxjs';
 
+import { RoutesService } from 'src/app/services/routes.service';
 import { CheckoutService } from 'src/app/services/checkout.service';
 import { CollectionService } from 'src/app/services/collection.service';
 import { LocalStorageUtils } from 'src/app/utils/localStorage';
@@ -9,7 +10,6 @@ import { LocalStorageUtils } from 'src/app/utils/localStorage';
 import Dog from 'src/app/models/dog';
 import Cart from 'src/app/models/cart';
 import DogCart from 'src/app/models/dogCart';
-import { concatMap, finalize, tap } from 'rxjs';
 
 @Component({
   selector: 'app-dog-details',
@@ -51,48 +51,50 @@ export class DogDetailsComponent {
       quantity: this.currentQuantity,
     };
 
-    this.collectionService.getById(this.dog.id!).subscribe({
-      next: (dog) => {
-        this.updatedDog = dog;
-        this.updatedDog.availableQuantity -= this.currentQuantity;
+    this.collectionService
+      .getById(this.dog.id!)
+      .pipe(
+        tap((dog: Dog) => this.updateDogInfo(dog)),
+        switchMap(() => this.collectionService.updateDog(this.updatedDog)),
+        switchMap(() => this.checkoutService.getCart(parseInt(this.localStorageUtils.getUserId()))),
+        tap((cart: Cart) => this.updateCartAndSummary(cart, dogCartAdd)),
+        switchMap(() => this.checkoutService.updateCart(this.updatedCart)),
+        finalize(() => this.routesService.navigateToCart())
+      )
+      .subscribe();
+  }
 
-        this.collectionService.updateDog(this.updatedDog).subscribe({
-          next: () => {
-            this.checkoutService
-              .getCart(parseInt(this.localStorageUtils.getUserId()))
-              .subscribe({
-                next: (cart) => {
-                  this.updatedCart = JSON.parse(JSON.stringify(cart));
+  updateDogInfo(dog: Dog): void {
+    this.updatedDog = { ...dog };
+    this.updatedDog.availableQuantity -= this.currentQuantity;
+  }
 
-                  let index = cart.dogs.findIndex(
-                    (dogCart) => dogCart.dogId == this.updatedDog.id
-                  );
+  updateCartAndSummary(cart: Cart, dogCartAdd: DogCart): void {
+    this.updateCart(cart, dogCartAdd);
+    this.updateSummary();
+  }
 
-                  if (index == -1) {
-                    this.updatedCart.dogs.push(dogCartAdd);
-                  } else {
-                    this.updatedCart.dogs[index].quantity +=
-                      this.currentQuantity;
-                  }
+  updateCart(cart: Cart, dogCartAdd: DogCart): void {
+    this.updatedCart = JSON.parse(JSON.stringify(cart));
 
-                  let sumToAdd = this.updatedDog.price! * this.currentQuantity;
+    let index = cart.dogs.findIndex(
+      (dogCart: DogCart) => dogCart.dogId == this.updatedDog.id
+    );
 
-                  this.updatedCart.summary += sumToAdd;
-                  this.updatedCart.total =
-                    this.updatedCart.summary - this.updatedCart.discount;
+    if (index == -1) {
+      this.updatedCart.dogs.push(dogCartAdd);
+    } else {
+      this.updatedCart.dogs[index].quantity += this.currentQuantity;
+    }
+  }
 
-                  this.updatedCart.total = +this.updatedCart.total.toFixed(2);
-                  this.updatedCart.summary =
-                    +this.updatedCart.summary.toFixed(2);
+  updateSummary(): void {
+    let sumToAdd = this.updatedDog.price! * this.currentQuantity;
 
-                  this.checkoutService.updateCart(this.updatedCart).subscribe({
-                    next: () => this.routesService.navigateToCart(),
-                  });
-                },
-              });
-          },
-        });
-      },
-    });
+    this.updatedCart.summary += sumToAdd;
+    this.updatedCart.total = this.updatedCart.summary - this.updatedCart.discount;
+
+    this.updatedCart.total = +this.updatedCart.total.toFixed(2);
+    this.updatedCart.summary = +this.updatedCart.summary.toFixed(2);
   }
 }
